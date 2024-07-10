89da8ca33f9636591de7037e1fc507367830e1b5
bennydan7 <baowusu-dankwah@st.ug.edu.gh>
2024-07-10 02:54:13 +0000

    created images component

---
+++ src/components/Images.js
@@ -0,0 +1,10 @@
+const Images = {
+  add_circle: require("../../assets/images/add_circle.png"),
+  ListView: require("../../assets/images/ListView.png"),
+  Logo: require("../../assets/images/Logo.png"),
+  Menu: require("../../assets/images/Menu.png"),
+  remove: require("../../assets/images/remove.png"),
+  Search: require("../../assets/images/Search.png"),
+  shoppingBag: require("../../assets/images/shoppingBag.png"),
+  Filter: require("../../assets/images/Filter.png"),
+};
